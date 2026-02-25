f90b5bc9fb6d8ff9fe028fb2053180797ec339c9
João Magno <162519556+joaomagno1@users.noreply.github.com>
2026-02-25 15:56:30 -0300

    1

---
+++ nest/src/cidade/service/cidade.service.create.ts
@@ -0,0 +1,32 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ConverterCidade } from '../dto/converter/cidade.converter';
+import { CidadeRequest } from '../dto/request/cidade.request';
+import { Cidade } from '../entity/cidade.entity';
+import { CidadeResponse } from '../dto/response/cidade.response';
+
+@Injectable()
+export class CidadeServiceCreate {
+  constructor(
+    @InjectRepository(Cidade)
+    private cidadeRepository: Repository<Cidade>,
+  ) {}
+
+  async create(cidadeRequest: CidadeRequest): Promise<CidadeResponse> {
+    let cidade = ConverterCidade.toCidade(cidadeRequest);
+
+    const cidadeCadastrada = await this.cidadeRepository
+      .createQueryBuilder('cidade')
+      .where('cidade.nomeCidade =:nome', { nome: cidade.nomeCidade })
+      .getOne();
+
+      if (cidadeCadastrada){
+        throw new HttpException('Cidade com nome informada já está cadastrada', HttpStatus.BAD_REQUEST);
+      }
+
+      cidade = await this.cidadeRepository.save(cidade);
+
+    return ConverterCidade.toCidadeResponse(cidade);
+  }
+}
